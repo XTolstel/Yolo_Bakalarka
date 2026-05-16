e5cd63a25a39e463aa57353161eb2932a2837821
Oleh Tolstel <160399128+XTolstel@users.noreply.github.com>
2026-05-17 00:12:10 +0200

    Simplify scripts to video object detection with bounding boxes only

--- new_yolo.py
+++ new_yolo.py
@@ -1,10 +1,7 @@
-import os
-HOME = os.path.expanduser("~")
+from Yolo_test import object_detection
 
-from roboflow import Roboflow
-rf = Roboflow(api_key="5HJFkhzx777bLHpOHCdo")
-project = rf.workspace("object-detection-yolo-bp").project("my-first-project-iwxqd")
-version = project.version(4)
-dataset = version.download("yolov5")
 
-project.version(4).deploy(model_type="yolov8", model_path=f"{HOME}/runs/detect/train4/")
+if __name__ == "__main__":
+    result = object_detection.detection()
+    print(f"Готово. Обработано кадров: {result['processed_frames']}")
+    print(f"Видео сохранено: {result['output_path']}")
